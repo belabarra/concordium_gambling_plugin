db75634959fb3df4cbdb15813aa50fc6b5c9f895
isabelabarra <isabela.codo-costa-barra20@imperial.ac.uk>
2025-10-26 10:30:56 +0000

    fixing issues

--- contracts/target/debug/deps/payout_contract-493770947991e9b6.d
+++
@@ -1,5 +0,0 @@
-/Users/isabelabarra/Desktop/Projects💫/concordium_gambling_plugin/contracts/target/debug/deps/payout_contract-493770947991e9b6.d: src/lib.rs
-
-/Users/isabelabarra/Desktop/Projects💫/concordium_gambling_plugin/contracts/target/debug/deps/libpayout_contract-493770947991e9b6.rmeta: src/lib.rs
-
-src/lib.rs:
--- contracts/target/debug/deps/payout_contract-4e691109d2b4892d.d
+++
@@ -1,5 +0,0 @@
-/Users/isabelabarra/Desktop/Projects💫/concordium_gambling_plugin/contracts/target/debug/deps/payout_contract-4e691109d2b4892d.d: src/lib.rs
-
-/Users/isabelabarra/Desktop/Projects💫/concordium_gambling_plugin/contracts/target/debug/deps/libpayout_contract-4e691109d2b4892d.rmeta: src/lib.rs
-
-src/lib.rs:
